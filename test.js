c5bc7eb5d6d77182c5a51e3e3e0f5759bf0681fd
Eric <eric.wu@yieldmo.com>
2015-12-09 17:28:06 -0500

    add more test

--- test.js
+++ test.js
@@ -1,12 +1,38 @@
-class User {
 
+class User {
    showName(){
       return "Eric";
   }
-
 }
-
 
 let user  = new User ;
 
 console.log(user.showName());
+
+
+function *gen(){
+  yield 1;
+  yield 2;
+  yield 3;
+  yield 4; 
+
+}
+
+let obj = {
+  [Symbol.iterator] (){
+    return gen();
+  }
+}
+
+for( let v of obj)
+  console.log(v);
+
+const GET_NAME = Symbol();
+
+class Person{
+    [GET_NAME](){
+        return  "you make it! cool";
+    }
+}
+let p = new Person();
+console.log(p[GET_NAME]());
